6bdc5c186c5b6c7bfa1eeba0de7feb926680e8cb
Rafael George <george.rafael@gmail.com>
2011-01-26 19:08:14 -0400

    initial commit

---
+++ lib/provider/ticket.rb
@@ -0,0 +1,12 @@
+module TicketMaster::Provider
+  module Versionone
+    # Ticket class for ticketmaster-versionone
+    #
+    
+    class Ticket < TicketMaster::Provider::Base::Ticket
+      #API = Versionone::Ticket # The class to access the api's tickets
+      # declare needed overloaded methods here
+      
+    end
+  end
+end
---
+++ lib/provider/versionone.rb
@@ -0,0 +1,25 @@
+module TicketMaster::Provider
+  # This is the Versionone Provider for ticketmaster
+  module Versionone
+    include TicketMaster::Provider::Base
+    #TICKET_API = Versionone::Ticket # The class to access the api's tickets
+    #PROJECT_API = Versionone::Project # The class to access the api's projects
+    
+    # This is for cases when you want to instantiate using TicketMaster::Provider::Versionone.new(auth)
+    def self.new(auth = {})
+      TicketMaster.new(:versionone, auth)
+    end
+    
+    # Providers must define an authorize method. This is used to initialize and set authentication
+    # parameters to access the API
+    def authorize(auth = {})
+      @authentication ||= TicketMaster::Authenticator.new(auth)
+      # Set authentication parameters for whatever you're using to access the API
+    end
+    
+    # declare needed overloaded methods here
+    
+  end
+end
+
+
