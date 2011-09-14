76887b2fe79314c901b66e62b81ddaba0df8d9a5
Rafael George <george.rafael@gmail.com>
2011-09-14 17:18:38 -0400

    added spec_helper and remove test directory

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'ticketmaster'
+require 'rspec'
+require 'ticketmaster-versionone'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  
+end
+
+def fixture_for(name, format = 'xml')
+  File.read(File.dirname(__FILE__) + "/fixtures/#{name}.#{format}")
+end
